b35c74bf24e0bd189cca3d4258426d106f65fca6
YoungHarry <prodharry@qq.com>
2023-09-18 18:53:55 +0800

    完善用户界面

--- func.py
+++ func.py
@@ -16,7 +16,7 @@ def time_value(dec):
         return get_str
     return wrapper
 
-#@st.cache_data
+@st.cache_data
 def load_car_names(fileName):   # 缓存:获取车辆类型
     # 构建文件路径
     file_Path = f'./_xlsx/{fileName}.xlsx'
@@ -27,9 +27,9 @@ def load_car_names(fileName):   # 缓存:获取车辆类型
     return work_excel.sheet_names
 
 # 缓存:获取对应车辆的可自定义功能
-#@st.cache_data
+@st.cache_data
 def load_table_data(fileName, carName):
-    print('i ma here')
+    print('load_table_data()!!!!')
     try:
         # 构建文件路径
         file_Path = f'./_xlsx/{fileName}.xlsx'
@@ -37,15 +37,17 @@ def load_table_data(fileName, carName):
             file_Path = f'{fileName}.xlsx'
         print('fileName: ', fileName, os.path.exists(file_Path))
         # 打开目标文件
-        df = pd.read_excel(file_Path,sheet_name=carName,header=3)
+        df = pd.read_excel(file_Path,sheet_name=carName,header=3,names=[
+            'name','can_data','rec_id','length','starting_byte','content_bits','multiple','offset',
+            'example','remark','value','type'])
         # 删除没有的CAN功能-> 根据 接收ID 是否为空判断
-        df = df.dropna(subset=['接收ID'])
+        df = df.dropna(subset=['rec_id'])
         # 重新分配行索引
         df = df.reset_index(drop=True)
         # 重新命名header
-        
-        df.iloc[:, 10] = df.iloc[:, 10].astype('str')
-        df.iloc[:, 1] = df.iloc[:, 1].astype('str')
+
+        df.iloc[:, 10] = df.iloc[:, 10].astype('str')   # value列
+        df.iloc[:, 1] = df.iloc[:, 1].astype('str')     # can_data 列
         return df
     except Exception as e:
         print('{0} load_table_data err:{1}'.format(time.strftime('[%Y-%m-%d-%H:%M:%S]'), str(e)))
@@ -71,7 +73,7 @@ def extract_number(text):   # 提取字符串中的数字部分为字符串
     match = re.search(r'\d+(\.\d+)?', text)
     if match:
         number_str = match.group()
-    
+
     return number_str
 
 def is_integer(s):  # 判断字符串否为整数
@@ -81,9 +83,11 @@ def is_integer(s):  # 判断字符串否为整数
     except ValueError:
         return False
 
+@st.cache_data
 def import_can(raw_table_data):# 导入can数据
     # 生成将要发送的can数据
     # 将示例变成字典存储
+    print('running import_can()')
     try:
         for pos, row in raw_table_data.iterrows():
             # 将示例转成字典存储
@@ -96,7 +100,7 @@ def import_can(raw_table_data):# 导入can数据
                 value = item[0] + '#' + can_Data_Format(item[1])
                 can_example_dict[key] = value
                 if index == 0 :     # 取得 第一个键值放入value 和 command
-                    default_key = key   
+                    default_key = key
                     default_value = value
             # 将列表插入单元格中
             raw_table_data.iloc[pos,8] = [can_example_dict]
@@ -106,7 +110,7 @@ def import_can(raw_table_data):# 导入can数据
 
             raw_table_data.iloc[pos,10] = default_key
             raw_table_data.iloc[pos,1] = default_value
-        return True
+        return raw_table_data
     except Exception as e:
         print('{0} import_can err:{1}'.format(time.strftime('[%Y-%m-%d-%H:%M:%S]'), str(e)))
 
--- main.py
+++ main.py
@@ -12,22 +12,26 @@ _car_options = load_car_names('can_data_all_model')
 car_option = st.selectbox('选择车型', _car_options, index= 0)
 print('选择车型:', car_option)
 
-# 获取原始数据
+# 缓存：获取原始数据
 raw_table_data = load_table_data('can_data_all_model', car_option)  # 原始数据
 
-# 导入CAN数据
-import_can(raw_table_data) # //
-print(raw_table_data)
+# 缓存：导入CAN数据
+raw_table_data = import_can(raw_table_data)
+
 
 # 显示对应车型的可自定义功能
-_func_options_list = list(raw_table_data.iloc[:,0])
-func_selected = st.multiselect('可自定义功能', options = _func_options_list,default= _func_options_list)
+def show_Enable_Func():
+    _func_options_list = list(raw_table_data.iloc[:,0])
+    st.multiselect('可自定义功能', options = _func_options_list,default= _func_options_list)
+    return True
 
+show_Enable_Func()
 
-def show_Edit():    # 初次显示编辑区
+def show_Edit(dataframe):    # 初次显示编辑区
+    print('running show_Edit')
     try:
     # 显示对应功能的修改模块
-        for pos, row in raw_table_data.iterrows():
+        for pos, row in dataframe.iterrows():
             cmd_type = row.iloc[11]
             cmd_name = row.iloc[0]
             cmd_value = row.iloc[10]
@@ -37,17 +41,54 @@ def show_Edit():    # 初次显示编辑区
                     cmd_value = int(cmd_value)
                 elif value_step == 0.01:    # 浮点型
                     cmd_value = float(cmd_value)
-                st.sidebar.number_input(cmd_name, step=value_step, value=cmd_value)
+                st.sidebar.number_input(cmd_name, step=value_step, value=cmd_value,key=cmd_name,on_change=input_callback,args=(cmd_name,))    # 可自定义输入
 
             elif cmd_type == ON_OFF_TYPE_FLAG: # 可选
                 cmd_options = row.iloc[8][0].keys() # 选项提取
-                st.sidebar.selectbox(cmd_name, options = cmd_options, index= 0)
+                st.sidebar.selectbox(cmd_name, options = cmd_options, index= 0,key=cmd_name,on_change=selected_callback,args=(cmd_name,))    # 可选
+        return True
     except Exception as e:
         print('{0} show_Edit err:{1}'.format(time.strftime('[%Y-%m-%d-%H:%M:%S]'), str(e)))
 
-show_Edit()
+
+
+def input_callback(cmd_name):
+    try:
+        cur_value = st.session_state[cmd_name]  # 获取对应的输入值
+        row_data = raw_table_data.loc[raw_table_data['name'] == cmd_name]
+
+        can_data = row_data['can_data']
+        starting_byte = int(row_data['starting_byte'])  # 开始字节
+        content_bits = int(row_data['content_bits'])    # 内容位数
+        multiple = float(row_data['multiple'])          # 倍数
+        offset = int(row_data['offset'])                # 偏移量
+        hex_string = format(int(cur_value/multiple),'x')
+        print('hex_string = ', hex_string)
+        # 根据规则生成can_data
+        #18DAF160#22 04 F3 04 61 04 90 04
+    except Exception as e:
+        print('{0} input_callback err:{1}'.format(time.strftime('[%Y-%m-%d-%H:%M:%S]'), str(e)))
+
+def selected_callback(cmd_name):
+    try:
+        cur_value = st.session_state[cmd_name]  # 对应的输入值
+        print('cur_value = ',cur_value)
+        #example = raw_table_data.loc[raw_table_data['name'] == cmd_name,'example'].values[0]
+        row = raw_table_data.loc[raw_table_data['name'] == cmd_name].index[0]   # 获取行
+        example = raw_table_data.iloc[row,8]
+        can_data = example[0][cur_value]
+        # 更新dataframe
+        raw_table_data.iloc[row,1] = can_data   # 更新can_data列
+        raw_table_data.iloc[row,10] = cur_value # 更新 value 列
+        print('update can_data:',can_data,';update value:', cur_value)
+
+    except Exception as e:
+        print('{0} selected_callback err:{1}'.format(time.strftime('[%Y-%m-%d-%H:%M:%S]'), str(e)))
+
+show_Edit(raw_table_data)
+
+
 # 显示表格
-st.dataframe(raw_table_data)
-
-
-
+st.dataframe(raw_table_data,use_container_width=True)
+print(raw_table_data)
+print(st.session_state)
